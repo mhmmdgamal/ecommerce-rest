555ffbf37400b34dfb94b3de8839f60b0a0d6543
Ahmed <Ahmed@Ahmed-PC>
2018-10-25 19:57:44 +0200

    reAdd lombok for project by Project -> clean

--- src/main/java/com/example/ecommerce/user/User.java
+++ src/main/java/com/example/ecommerce/user/User.java
@@ -22,19 +22,4 @@ public class User {
    
     private String email;
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
 }
